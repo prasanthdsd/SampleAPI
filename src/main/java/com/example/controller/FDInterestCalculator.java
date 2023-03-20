fb94003659e3eed01d9835bdf9c87fe64fba00e2
prasanthdsd <pkprasanth54@gmail.com>
2023-03-20 15:10:27 +0530

    Update FDInterestCalculator.java

--- src/main/java/com/example/controller/FDInterestCalculator.java
+++ src/main/java/com/example/controller/FDInterestCalculator.java
@@ -11,7 +11,6 @@ import java.util.List;
 
 import javax.validation.Valid;
 
-import org.checkerframework.common.value.qual.StringVal;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
